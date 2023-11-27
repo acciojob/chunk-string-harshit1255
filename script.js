62471d10c4a98df6e8f9fd27f13c4020ce38e320
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-27 05:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	let arr=[]
+	let count  = parseInt(str.length/size);
+	let j=0;
+	let rem =str.legth%size;
+	for(let i=0;i<count;i++)
+		{
+			arr.push(str.substr(j,5));
+			j+=size;
+		}
+	if(rem!==0)
+	{
+		arr.push(str.substr(j,5));
+	}
+	return arr;
 }
 
 // Do not change the code below
